f132a9d78505dcc99df1777ecb878b76f2fc8cad
bubas3000 <47362581+bubas3000@users.noreply.github.com>
2020-08-17 17:44:12 +0100

    Add files via upload

---
+++ PiCo.php
@@ -0,0 +1,362 @@
+<?php include 'sendemail.php'; ?>
+<!DOCTYPE HTML>
+<!--
+	Story by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>VIZER</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="divided">
+
+				<!-- One -->
+					<section class="banner style5 orient-left content-align-center image-position-center fullscreen ">
+						<div class="content">
+							<h1></h1>
+							<p class="major"></p>
+							<!-- <ul class="actions stacked">
+								<li><a href="#first" class="button big wide smooth-scroll-middle">Get Started</a></li>
+							</ul> -->
+						</div>
+						<div class="image">
+							<img src="images/Bernardo.jpg" width = "100%" height = "100%" alt="" />
+						</div>
+						<div class="image2">
+							<img src="images/logo.jfif"  alt="" />
+						</div>
+					</section>
+
+				<!-- Two -->
+					<section class="spotlight style1 orient-right content-align-left image-position-center onscroll-image-fade-in" id="first">
+						<div class="image">
+							<img src="images/Aerea.png" alt="" />
+						</div>
+						<div class="content">
+							<h2>Mapeamento Aéreo</h2>
+							<p>Na Vizer, aliamos a <b>rapidez</b> de uma imagem aérea ao <b>detalhe</b> de uma observação em pessoa.</p>
+							<p>Faça o levantamento de uma área de <b>difícil acesso</b>, encontre pontos de <b>escoamento pluvial,</b> confirme o <b>resultado de intervenções</b> florestais ou <b>avalie os danos causados por meteorologia</b> hostil. Tudo em apenas algumas horas de voo!</p>
+							<p>Para que possa ser ainda mais preciso na sua gestão agrícola, disponibilizamos a opção de processamento das imagens capturadas em voo. Obtenha todos os dados (áreas, distâncias, contagens de árvores…) de que necessita.</p>
+							<ul class="actions stacked">
+								<li><a href="#forth" class="button big wide smooth-scroll-middle">Pedir Orçamento</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section class="spotlight style1 orient-left content-align-left image-position-center onscroll-image-fade-in" id="second">
+						<div class="image">
+							<img src="images/Aereacor.png" alt="" />
+						</div>
+						<div class="content">
+							<h2>Análise de Culturas</h2>
+							<p>Sabemos como é importante <b>acompanhar de perto as culturas</b> que gere.</p>
+                            <p>O problema é que, normalmente, só consegue ir perto dos limites exteriores, onde a estrada permite, o que <b>pode não ser suficientemente perto</b>. E se pudesse ver tudo? À distância que quisesse?</p>
+                            <p>Na Vizer, trabalhamos para que possa acompanhar as suas culturas na totalidade, identificando zonas de menor crescimento e zonas alagadas em tempo útil. Faça-o com um vídeo em tempo real ou comparando imagens tiradas regularmente da mesma cultura.
+                            </p>
+							<ul class="actions stacked">
+								<li><a href="#forth" class="button big wide smooth-scroll-middle">Pedir Orçamento</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Four -->
+				<!--	<section class="spotlight style5 orient-right content-align-left image-position-center onscroll-image-fade-in" id="third">
+						<div class="content">
+							<h2>Vídeos Promocionais de Explorações Agro-florestais</h2>
+							<p>Ofereça um vídeo aos seus clientes, fornecedores ou investidores, mostrando o trabalho que desenvolve enquanto gestor agrícola.
+
+								Sobrevoamos qualquer tipo de área, barragens, gado para obter imagens (fotografia e vídeo). Serviço inclui tratamento da imagem.</p>
+							<ul class="actions stacked">
+								<li><a href="#" class="button big wide smooth-scroll-middle">Learn More</a></li>
+							</ul>
+						</div>
+						<div class="image" style="overflow: hidden">
+							<video autoplay loop muted inline width = "100%" >
+								<source src="Videosite.webm" type="video/webm">
+								<source src="Videosite.mp4" type="video/mp4">
+								<img src="Videosite.gif"	/>
+							</video>
+						</div>
+					</section>
+				-->
+				<!-- Five -->
+					<section class="wrapper style1 align-center">
+						<div class="inner">
+							<h2>Fotografias e Vídeos Promocionais de Explorações Agro-florestais</h2>
+							<p>Muitas vezes, é tão importante ser um bom gestor <b>como mostrar uma boa gestão.</b>
+
+                                Ofereça um vídeo aos seus clientes, fornecedores ou investidores, mostrando barragens, gado, área florestal e agrícola.
+                            </div>
+						
+
+						<!-- Gallery -->
+							<div class="gallery style2 medium lightbox">
+								<article>
+									<a href="images/DJI_0195lado.jpg" class="image">
+										<img src="images/DJI_0195lado.jpg" alt="" />
+									</a>
+								</article>
+								<article>
+									<a href="images/DJI_0588lado.jpg" class="image">
+										<img src="images/DJI_0588lado.jpg" alt="" />
+									</a>
+								</article>
+								<article>
+									<a href="images/DJI_0891lado.jpg" class="image">
+										<img src="images/DJI_0891lado.jpg" alt="" />
+									</a>
+								</article>
+								<article>
+									<a href="images/Teste1.jpg" class="image">
+										<a href="images/gallery/fulls/06.jpg" class="image">
+											<video autoplay loop muted inline width = "100%" >
+												<source src="Vacas.webm" type="video/webm">
+												<source src="Vacas.mp4" type="video/mp4">
+												<img src="Vacas.gif"	/>
+											</video>
+										</a>
+									</a>
+								</article>
+								<article>
+									<a href="images/Teste1.jpg" class="image">
+										<img src="images/Teste1.jpg" alt="" />
+									</a>
+								</article>
+								<!--<article>
+									<a href="images/gallery/fulls/07.jpg" class="image">
+										<img src="images/gallery/thumbs/07.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Ipsum Lorem</h3>
+										<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/gallery/fulls/08.jpg" class="image">
+										<img src="images/gallery/thumbs/08.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Magna Risus</h3>
+										<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/gallery/fulls/09.jpg" class="image">
+										<img src="images/gallery/thumbs/09.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Tempus Dolor</h3>
+										<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/gallery/fulls/10.jpg" class="image">
+										<img src="images/gallery/thumbs/10.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Sed Etiam</h3>
+										<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/gallery/fulls/11.jpg" class="image">
+										<img src="images/gallery/thumbs/11.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Magna Lorem</h3>
+										<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								<article>
+									<a href="images/gallery/fulls/12.jpg" class="image">
+										<img src="images/gallery/thumbs/12.jpg" alt="" />
+									</a>
+									<div class="caption">
+										<h3>Ipsum Dolor</h3>
+										<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+										<ul class="actions fixed">
+											<li><span class="button small">Details</span></li>
+										</ul>
+									</div>
+								</article>
+								!-->
+							</div>
+
+					</section>
+				
+				<!-- Six -->
+                    <!--<section class="wrapper style1 align-center">
+						<div class="inner">
+							<h2>Contacte-nos</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum.</p>
+							<div class="items style1 medium onscroll-fade-in">
+								<section>
+									<span class="icon style2 major fa-envelope"></span>
+									<h3>Mail</h3>
+									<p>vizer.drones@gmail.com</p>
+								</section>
+								<section>
+									<span class="icon brands style2 major fa-facebook-f"></span>
+									<h3>Facebook</h3>
+									<p>Vizer Drones</p>
+								</section>
+								<section>
+									<span class="icon brands style2 major fa-linkedin-in"></span>
+									<h3>Linkedin</h3>
+									<p>Vizer Drones</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-wifi"></span>
+									<h3>Amet</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-cog"></span>
+									<h3>Magna</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon style2 major fa-paper-plane"></span>
+									<h3>Tempus</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-desktop"></span>
+									<h3>Aliquam</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-sync-alt"></span>
+									<h3>Elit</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-hashtag"></span>
+									<h3>Morbi</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-bolt"></span>
+									<h3>Turpis</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-envelope"></span>
+									<h3>Ultrices</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+								<section>
+									<span class="icon solid style2 major fa-leaf"></span>
+									<h3>Risus</h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+								</section>
+							</div>
+						</div>
+					</section>
+				-->
+				<!-- Seven -->
+				<!--	
+					<section class="wrapper style1 align-center">
+						<script language="JavaScript" src="scripts/gen_validatorv31.js" type="text/javascript"></script>-->
+						<!--alert messages start-->
+						<!--<?php echo $alert; ?>-->
+						<!--alert messages end-->
+					<!--	
+						<div class="inner medium">
+							<h2>Contacte-nos</h2>
+							<form method="post" action="">
+								<div class="fields">
+									<div class="field half">
+										<label for="name">Nome</label>
+										<input type="text" name="name" id="name" value="" />
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input type="email" name="email" id="email" value="" />
+									</div>
+									<div class="field">
+										<label for="message">Messagem</label>
+										<textarea name="message" id="message" rows="6"></textarea>
+									</div>
+								</div>
+								<ul class="actions special">
+									<li><input type="submit" name="submit" id="submit" value="Enviar" /></li>
+								</ul>
+							</form>
+							<script type="text/javascript">
+							if(window.history.replaceState){
+							window.history.replaceState(null, null, window.location.href);
+							}
+							</script>-->
+							<!--<script language="JavaScript">
+								// Code for validating the form
+								// Visit http://www.javascript-coder.com/html-form/javascript-form-validation.phtml
+								// for details
+								var frmvalidator  = new Validator("myemailform");
+								frmvalidator.addValidation("name","req","Please provide your name"); 
+								frmvalidator.addValidation("email","req","Please provide your email"); 
+								frmvalidator.addValidation("email","email","Please enter a valid email address"); 
+							</script>
+							-->
+						<!--</div>
+					</section>-->
+                    <section class="spotlight style1 orient-left content-align-left image-position-center onscroll-image-fade-in" id="forth">
+						<div class="image">
+							<img src="images/Team.jfif" alt="" />
+						</div>
+						<div class="content">
+							<h2>Contacte-nos</h2>
+							<p>Preciso de um texto para aqui</p>
+                            <p><b>Email:</b>vizer.drones@gmail.com</p>
+                            <p><b>Telemóvel:</b>+351915988611</p>
+						</div>
+					</section>
+				<!-- Footer -->
+					<footer class="wrapper style1 align-center">
+						<div class="inner">
+							<ul class="icons">
+								<li><a href="https://www.facebook.com/VizerAgriculturadePrecisao/" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.linkedin.com/company/vizer-precision-agriculture" class="icon brands style2 fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+								<li><a href="mailto:vizer.drones@gmail.com" class="icon style2 fa-envelope"><span class="label">Email</span></a></li>
+							</ul>
+							<p>&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
